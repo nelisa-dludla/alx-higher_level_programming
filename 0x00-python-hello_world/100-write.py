e4ec40036c49d75dc02470f6daf2770d8f99aa01
Nelisa Dludla <nelisa.dludla@outlook.com>
2023-10-03 01:55:34 +0200

    Updated the format for task 11 code

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 '''
-This script prints a string to stderr using the write function from the sys module and exits with a status of 1
+This script prints a string to stderr using the write function
+from the sys module and exits with a status of 1
 '''
 
 import sys
 
 sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
 sys.exit(1)
-
