5870ee2a0b8514abd8157602f10d4e03c10fd037
Nelisa Dludla <nelisa.dludla@outlook.com>
2023-10-05 23:28:33 +0200

    Updated task 0 code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
-'''
-This script imports a function def add(a, b): from the file
-add_0.py and prints the result of the addition 1 + 2 = #!/usr/bin/env python3
-'''
-
 
 if __name__ == '__main__':
+    '''
+    This script imports a function def add(a, b): from the file
+    add_0.py and prints the result of the addition 1 + 2 = #!/usr/bin/env python3
+    '''
+
     from add_0 import add
 
     a = 1
     b = 2
 
-    print('{} + {} = {}'.format(a, b, add(a, b)))
+print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
