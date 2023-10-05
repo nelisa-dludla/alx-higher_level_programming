cb8211b2296aac6d360b3896a63e065291e166dc
Nelisa Dludla <nelisa.dludla@outlook.com>
2023-10-05 23:38:09 +0200

    Updated task 0 code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,10 +5,8 @@ if __name__ == '__main__':
     add_0.py and prints the result of the addition 1 + 2 = 3
     '''
     from add_0 import add
-
     a = 1
     b = 2
     sum = 0
     sum += add(a, b)
-
     print('{:d} + {:d} = {:d}'.format(a, b, sum))
