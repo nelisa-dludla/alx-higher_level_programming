ecc9a448c2db0a782cea416a061612f874d5e389
Nelisa Dludla <nelisa.dludla@outlook.com>
2023-10-06 00:35:45 +0200

    Updated task 1 code

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Function does some Maths, and prints the result"""
+    """Does addition, substraction, multiplication and division"""
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
