a191eb09065f5a7bf45d992269121c4ae35b6167
Nelisa Dludla <nelisa.dludla@outlook.com>
2023-10-06 00:01:06 +0200

    Updated task 0 - quotes

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """This script imports add and uses it"""
+if __name__ == '__main__':
+    '''This script imports add and uses it'''
     from add_0 import add
     a = 1
     b = 2
     sum = 0
     sum += add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, sum))
+    print('{:d} + {:d} = {:d}'.format(a, b, sum))
