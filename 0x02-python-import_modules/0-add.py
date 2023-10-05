8482af44d2cd49b5404a760561d6a632c12baa4c
Nelisa Dludla <nelisa.dludla@outlook.com>
2023-10-06 00:02:56 +0200

    Updated task 0 code - removed sum variable

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,6 +4,4 @@ if __name__ == '__main__':
     from add_0 import add
     a = 1
     b = 2
-    sum = 0
-    sum += add(a, b)
-    print('{:d} + {:d} = {:d}'.format(a, b, sum))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
