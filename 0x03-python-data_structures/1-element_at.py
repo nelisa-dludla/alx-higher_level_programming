fb48e8ad3ee747225e0751be55f6ea59c0e95e1b
Nelisa Dludla <nelisa.dludla@outlook.com>
2023-10-07 12:21:45 +0200

    Updated task 1 code

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,7 +5,7 @@ Retrieves an element from a list like in C
 
 
 def element_at(my_list, idx):
-    if not idx or idx > len(my_list) - 1:
+    if idx < 0 or idx > len(my_list) - 1:
         return None
     else:
         return my_list[idx]
