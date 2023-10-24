3403830a55f49d7c79e8ee071abe3d1f5af2b3ab
nelisa-dludla <nelisa.dludla@outlook.com>
2023-10-25 01:59:27 +0200

    Updated task 1 code

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
+'''
+This class defines a square.
+'''
 
 class Square:
     '''
-    This class defines a square
+    This class defines a square.
+
+    Attributes:
+        size (int): The size of the square.
+
+    Methods:
+        __init__(self, size)): Initializes a new Square instance.
     '''
+
     def __init__(self, size):
+        '''
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        '''
+
         self.__size = size
