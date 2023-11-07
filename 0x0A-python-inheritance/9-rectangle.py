0bf54d4bd7a5c7fbf2931538b4d3876313dd94e9
nelisa-dludla <nelisa.dludla@outlook.com>
2023-11-07 02:24:07 +0200

    Added task 10 solution and updated task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle(BaseGeometry):
     A subclass that inherits from BaseGeometry
     '''
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
 
         self.__width = width
         self.__height = height
