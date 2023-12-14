964531ca2466cc86ee73e7434ceb0d70e122428a
Nelisa Dludla <nelisa.dludla@outlook.com>
2023-12-14 13:26:48 +0200

    Added solutions to tasks 14 - 16 and updated tasks 11 - 13 scripts

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- This script lists all shows contained in hbtn_0d_tvshows
-SELECT tv_shows.title, COALESCE(tv_show_genres.genre_id, 'NULL') as genre_id
+SELECT tv_shows.title, COALESCE(tv_show_genres.genre_id, 'NULL') AS genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
