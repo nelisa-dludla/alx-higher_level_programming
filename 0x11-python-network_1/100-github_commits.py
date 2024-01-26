82cd3caee2082b584377dd68f6d96b7bd7f6fda0
Nelisa Dludla <nelisa.dludla@outlook.com>
2024-01-27 01:52:59 +0200

    Added solution to advanced task - produces incorrect output

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+This script solves an interview question
+'''
+
+from requests import get
+from sys import argv
+
+if __name__ == '__main__':
+    repo_name = argv[1]
+    owner_name = argv[2]
+
+    url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
+    res = get(url)
+    commits = res.json()
+    sorted_commits = sorted(commits, key=lambda x: x['commit']['author']['date'], reverse=True)
+
+    for commit in sorted_commits[:10]:
+        sha = commit.get('sha')
+        author_name = commit.get('commit').get('author').get('name')
+        print(f'{sha}: {author_name}')
